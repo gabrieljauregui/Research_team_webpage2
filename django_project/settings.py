354d60fef0909566fbee314c9e559b6a03ff1f69
Gabi <gabriel.jaur@gmail.com>
2023-02-24 20:39:01 -0300

    pasando a notebook para trabajar

--- django_project/settings.py
+++ django_project/settings.py
@@ -15,19 +15,19 @@ from decouple import config
 from django.conf import settings
 import os
 from django.contrib.messages import constants as messages
+
 settings.configure()
 
 MESSAGE_TAGS = {
-        messages.DEBUG: 'alert-secondary',
-        messages.INFO: 'alert-info',
-        messages.SUCCESS: 'alert-success',
-        messages.WARNING: 'alert-warning',
-        messages.ERROR: 'alert-danger',
- }
+    messages.DEBUG: "alert-secondary",
+    messages.INFO: "alert-info",
+    messages.SUCCESS: "alert-success",
+    messages.WARNING: "alert-warning",
+    messages.ERROR: "alert-danger",
+}
 
 
-MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
-
+MESSAGE_STORAGE = "django.contrib.messages.storage.session.SessionStorage"
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -61,7 +61,6 @@ INSTALLED_APPS = [
     "django_extensions",
     "muns_app",
     "accounts",
-    
 ]
 
 MIDDLEWARE = [
@@ -100,6 +99,20 @@ WSGI_APPLICATION = "django_project.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "railway",
+        "USER": "postgres",
+        "PASSWORD": "qLqUXZ1E4y8aDlR0TG0d",
+        "HOST": "containers-us-west-92.railway.app",
+        "PORT": "7812",
+    }
+}
+
+
+"""
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
@@ -110,6 +123,8 @@ DATABASES = {
         "PORT": "5432",
     }
 }
+
+"""
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
