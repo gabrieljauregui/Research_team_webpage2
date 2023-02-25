3bd62b58dc99a81dc4e1f83a29e565fd24428185
Gabi <gabriel.jaur@gmail.com>
2023-02-24 21:25:07 -0300

    pasando a notebook para trabajar

--- django_project/settings.py
+++ django_project/settings.py
@@ -173,6 +173,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 AUTH_USER_MODEL = "accounts.MyUser"
 
+DEFAULT_CHARSET = "utf8"
 
 LOGIN_REDIRECT_URL = "home"
 LOGOUT_REDIRECT_URL = "home"
