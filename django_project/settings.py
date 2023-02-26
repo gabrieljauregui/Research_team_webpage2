2fba9322a8b67cbff17908ff3a3cdd95c5c8880b
Gabi <gabriel.jaur@gmail.com>
2023-02-26 17:42:22 -0300

    testing cortexculture.com 2

--- django_project/settings.py
+++ django_project/settings.py
@@ -15,7 +15,7 @@ from decouple import config
 from django.conf import settings
 import os
 from django.contrib.messages import constants as messages
-
+import dj_database_url
 
 settings.configure()
 
@@ -105,6 +105,13 @@ WSGI_APPLICATION = "django_project.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+DATABASE_URL = "PGPASSWORD=tVU0PW3bvIidOzsEQdQU psql -h containers-us-west-18.railway.app -U postgres -p 7231 -d railway"
+
+
+DATABASES = {
+    "default": dj_database_url.config(default=DATABASE_URL, conn_max_age=1800),
+}
+"""
 
 DATABASES = {
     "default": {
@@ -118,7 +125,9 @@ DATABASES = {
 }
 
 
-"""
+
+
+
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
