7d8f8c265323be46f408193afcd244c575f5793c
Gabi <gabriel.jaur@gmail.com>
2023-02-26 19:47:18 -0300

    testing alone and sad again, lol, jk, deleted mysql from requirements.txt

--- django_project/settings.py
+++ django_project/settings.py
@@ -15,7 +15,7 @@ from decouple import config
 from django.conf import settings
 import os
 from django.contrib.messages import constants as messages
-from django import dj_database_url
+import dj_database_url
 
 settings.configure()
 
